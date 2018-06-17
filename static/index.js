e8c0e646394bcb12be262dc3484e62c28b042136
morch028 <morch028@uottaw.ca>
2018-06-16 23:44:45 -0400

    Adds support to edit command details
    The edit card is now populated with sliders that can affect the various
    aspects of a motor maneuver.

--- static/index.js
+++ static/index.js
@@ -1,7 +1,21 @@
 let CURRENT_MODE;
+let SEND_COMMANDS_URL;
+
+const COMMAND_MAP = { //🢀🢂🢁🢃🢄🢅🢆🢇■
+    arc_left:       {icon: "🢄", text: "Arc Left", color: "", details: {speed_left: 50, speed_right: 200, duration: 1.5}},
+    forward:        {icon: "🢁", text: "Forward", color: "", details: {speed_left: 150, speed_right: 150, duration: 1}},
+    arc_right:      {icon: "🢅", text: "Arc Right", color: "", details: {speed_left: 50, speed_right: 200, duration: 1.5}},
+    left:           {icon: "🢀", text: "Left", color: "", details: {speed_left: -150, speed_right: 150, duration: 1}},
+    stop:           {icon: "■", text: "Stop", color: "", details: {speed_left: 0, speed_right: 0, duration: 2}},
+    right:          {icon: "🢂", text: "Right", color: "", details: {speed_left: 150, speed_right: -150, duration: 1}},
+    back_arc_left:  {icon: "🢆", text: "Back Arc Left", color: "", details: {speed_left: -50, speed_right: -200, duration: 1.5}},
+    backwards:      {icon: "🢃", text: "Backwards", color: "", details: {speed_left: 150, speed_right: 150, duration: 1}},
+    back_arc_right: {icon: "🢇", text: "Back Arc Right", color: "", details: {speed_left: -150, speed_right: -50, duration: 1.5}}
+};
 
 function main(){
     selectMode("edit");
+    initializeSliders();
     $("#modeSwitchContainer button").click(event => {
             const $button = $(event.target).closest(".btn");
             selectMode($button.data("mode"));
@@ -13,7 +27,7 @@ function main(){
 
 function clickCommand(commandKey) {
     if (CURRENT_MODE === "edit") {
-        openEditForCommand(commandKey);
+        openCommandEdit(commandKey);
     } else if (CURRENT_MODE === "auto") {
         sendCommand(commandKey);
     } else if (CURRENT_MODE === "plan") {
@@ -23,13 +37,17 @@ function clickCommand(commandKey) {
     }
 }
 
-function openEditForCommand(commandKey) {
-    alert("Not yet implemented");
+function openCommandEdit(commandKey) {
+    const command = COMMAND_MAP[commandKey];
+    const $editCard = $("#editCard");
+    $editCard.find(".card-header h2").text(`${command.text}  ${command.icon}`);
+    $editCard.data("command", commandKey);
+    setCommandDetailSliders(command.details);
 }
 
 function sendCommand(command) {
     if (typeof command === "string") {
-        alert("Not yet implemented");
+        sendCommands([COMMAND_MAP[command]]);
     } else if (typeof command === "object") {
         sendCommands([command]);
     }
@@ -40,7 +58,23 @@ function addCommandToTimeline(commandKey) {
 }
 
 function sendCommands(commandList) {
-    alert("Not yet implemented");
+    console.table(commandList);
+    $.ajax({
+        method: "POST",
+        url: SEND_COMMANDS_URL,
+        contentType: 'application/json',
+        data: JSON.stringify({moves: commandList})
+    }).done(() => {
+        console.log("Sent commands successfully");
+    }).fail(error => {
+        console.error(`Failed to send commands with error: ${error}`);
+    })
+}
+
+function setCommandDetailSliders(command) {
+    $("#speedLeft").slider("value", command.speed_left);
+    $("#speedRight").slider("value", command.speed_right);
+    $("#duration").slider("value", command.duration);
 }
 
 async function selectMode(mode) {
@@ -69,6 +103,37 @@ function highlightModeButton(mode) {
     $(selectedButton).addClass("btn-primary").removeClass("btn-dark");
 }
 
+
+function initializeSliders() {
+    $(".command-field-speed").slider({
+        value: 0,
+        min: -255,
+        max: 255,
+        step: 5,
+        animate: "fast",
+        slide: (event, ui) => $(ui.handle).text(ui.value),
+        change: (event, ui) => sliderValueChanged($(event.target), $(ui.handle), ui.value)
+
+    });
+    $(".command-field-duration").slider({
+        value: 1,
+        min: 0.1,
+        max: 5.0,
+        step: 0.1,
+        animate: "fast",
+        slide: (event, ui) => $(ui.handle).text(ui.value),
+        change: (event, ui) => sliderValueChanged($(event.target), $(ui.handle), ui.value)
+    });
+    openCommandEdit("stop");
+}
+
+function sliderValueChanged($slider, $handle, value) {
+    $handle.text(value);
+    const commandKey = $slider.parents(".card").data("command");
+    const command = COMMAND_MAP[commandKey];
+    command.details[$slider.data("detailKey")] = value;
+
+}
 
 function toggleEditPanel(showEditPanel) {
     return new Promise(resolve => {
