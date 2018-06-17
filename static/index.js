93e8af80417e50ba328a3125ba2d169cbeebdcc8
morch028 <morch028@uottaw.ca>
2018-06-16 21:13:31 -0400

    Prevents mode changes during mode change animation
    Adds promise handling to animation functions to prevent spam clicking
    the mode options. Also adds some basic content to the timeline.

--- static/index.js
+++ static/index.js
@@ -1,14 +1,62 @@
-function selectMode(mode) {
-    console.log("Selecting mode: " + mode);
-    highlightModeButton(mode);
-    if (mode === "edit") {
-        enterEditMode();
-    } else if (mode === "auto") {
-        enterAutoMode();
-    } else if (mode === "plan") {
-        enterPlanMode();
+let CURRENT_MODE;
+
+function main(){
+    selectMode("edit");
+    $("#modeSwitchContainer button").click(event => {
+            const $button = $(event.target).closest(".btn");
+            selectMode($button.data("mode"));
+    });
+    $(".command-button").click(event => {
+        clickCommand($(event.target).data("commandKey"));
+    });
+}
+
+function clickCommand(commandKey) {
+    if (CURRENT_MODE === "edit") {
+        openEditForCommand(commandKey);
+    } else if (CURRENT_MODE === "auto") {
+        sendCommand(commandKey);
+    } else if (CURRENT_MODE === "plan") {
+        addCommandToTimeline(commandKey);
     } else {
-        console.error("Unrecognized Mode: " + mode);
+        throw new Error(`Current mode in illegal state: ${CURRENT_MODE}`);
+    }
+}
+
+function openEditForCommand(commandKey) {
+    alert("Not yet implemented");
+}
+
+function sendCommand(command) {
+    if (typeof command === "string") {
+        alert("Not yet implemented");
+    } else if (typeof command === "object") {
+        sendCommands([command]);
+    }
+}
+
+function addCommandToTimeline(commandKey) {
+    alert("Not yet implemented");
+}
+
+function sendCommands(commandList) {
+    alert("Not yet implemented");
+}
+
+async function selectMode(mode) {
+    if (CURRENT_MODE !== mode) {
+        console.log(`Selecting mode: ${mode}`);
+        highlightModeButton(mode);
+        if (mode === "edit") {
+            await enterEditMode();
+        } else if (mode === "auto") {
+            await enterAutoMode();
+        } else if (mode === "plan") {
+            await enterPlanMode();
+        } else {
+            throw new Error(`Unrecognized mode: ${mode}`);
+        }
+        CURRENT_MODE = mode;
     }
 }
 
@@ -21,46 +69,39 @@ function highlightModeButton(mode) {
     $(selectedButton).addClass("btn-primary").removeClass("btn-dark");
 }
 
-function enterEditMode() {
-    toggleTimeline(false);
-    toggleEditPanel(true)
-}
-
-function enterAutoMode() {
-    toggleTimeline(false);
-    toggleEditPanel(false);
-}
-
-function enterPlanMode() {
-    toggleEditPanel(false);
-    toggleTimeline(true);
-}
-
 
 function toggleEditPanel(showEditPanel) {
-    if (showEditPanel) {
-        $("#commandButtonContainer").switchClass("col-md-12", "col-xl-6 col-lg-7", {
-            complete: () => {$("#editCardContainer").slideDown()}
-        });
-    } else {
-        $("#editCardContainer").slideUp(() => {
-            $("#commandButtonContainer").switchClass("col-xl-6 col-lg-7", "col-md-12");
-        });
-    }
+    return new Promise(resolve => {
+        if (showEditPanel) {
+            $("#commandButtonContainer").switchClass("col-md-12", "col-xl-6 col-lg-7",
+                {complete: () => $("#editCardContainer").slideDown(resolve)}
+            );
+        } else {
+            $("#editCardContainer").slideUp(() => {
+                $("#commandButtonContainer").switchClass("col-xl-6 col-lg-7", "col-md-12", {complete: resolve});
+            });
+        }
+    });
 }
 
 function toggleTimeline(showTimeline) {
-    if (showTimeline) {
-        $("#timelineContainer").slideDown();
-    } else {
-        $("#timelineContainer").slideUp();
-    }
+    return new Promise(resolve => {
+        if (showTimeline) {
+            $("#timelineContainer").slideDown(resolve);
+        } else {
+            $("#timelineContainer").slideUp(resolve);
+        }
+    });
 }
 
-function main(){
-    selectMode("edit");
-    $("#modeSwitchContainer button").click(event => {
-            const $button = $(event.target).closest(".btn");
-            selectMode($button.data("mode"));
-    });
+function enterEditMode() {
+    return Promise.all([toggleTimeline(false), toggleEditPanel(true)]);
+}
+
+function enterAutoMode() {
+    return Promise.all([toggleTimeline(false), toggleEditPanel(false)]);
+}
+
+function enterPlanMode() {
+    return Promise.all([toggleEditPanel(false), toggleTimeline(true)]);
 }
